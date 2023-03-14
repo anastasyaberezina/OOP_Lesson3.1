685c19588fd3a7c93a8a4d65aacdfdfe59431b3f
Anastasiya B <gavrilovaanastasya@yandex.ru>
2023-03-14 10:00:26 +0300

    add window

--- Window.java
+++ Window.java
@@ -1,2 +1,22 @@
-package DailyPlanner.DPlanner;public class Window {
+package DailyPlanner;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Window {
+    protected JFrame frame;
+    protected JPanel panel;
+    public Window (int x, int y){
+        panel = JPanel();
+        frame = JFrame("Планировщик заданий");
+        panel.setPreferredSize(new Dimension(x,y));
+        frame.add(panel);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        panel.setBackground(color);
+    }
 }
+
